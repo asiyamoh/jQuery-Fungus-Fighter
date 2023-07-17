b29868f244cff932839eb8337be1609f9c980483
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-07-17 17:52:05 -0500

    made a to do list

--- script.js
+++ script.js
@@ -3,16 +3,26 @@ $(document).ready(onReady);
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+//let fungusHP = 100;
+
+let fungusHP = 100;
+let attackAP = 100;
 
 function onReady() {
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
+    $('.attacks').on('click', humanAttack);
     
     
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+}
+
+function humanAttack(){
+    console.log('Good Job!');
+    let losePoints = 
+
 }
